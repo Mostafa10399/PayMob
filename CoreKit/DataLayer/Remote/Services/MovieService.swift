7b6ace149e8ef08e209c2bc5077d88a5edfdcea2
Mostafa10399 <mostafaelshall972@gmail.com>
2025-04-25 19:03:50 +0300

    create movies Apis

---
+++ CoreKit/DataLayer/Remote/Services/MovieService.swift
@@ -0,0 +1,69 @@
+//
+//  MovieService.swift
+//  CoreKit
+//
+//  Created by Mostafa on 25/04/2025.
+//
+
+import Foundation
+import Alamofire
+
+public enum MovieService {
+    case popular(auth: String, page: Int? = nil, region: String? = nil, language: String? = nil)
+    case movie(auth: String, id: Int, language: String? = nil)
+    case similar(auth: String, id: Int, page: Int? = nil, language: String? = nil)
+}
+
+extension MovieService: MoviesDBService {
+    public var mainRoute: String { return "movie/" }
+
+    public var requestConfiguration: RequestConfiguration {
+        switch self {
+        case let .popular(auth, page, region, language):
+            var parameters: [String: Any] = ["api_key": auth]
+            if let page = page {
+                parameters["page"] = page
+            }
+            if let region = region {
+                parameters["region"] = region
+            }
+            if let language = language {
+                parameters["language"] = language
+            }
+            return RequestConfiguration(
+                method: .get,
+                path: mainRoute.appending("popular"),
+                parameters: parameters,
+                encoding: URLEncoding.queryString,
+                language: language
+            )
+        case let .movie(auth, id, language):
+            var parameters: [String: Any] = ["api_key": auth]
+            if let language = language {
+                parameters["language"] = language
+            }
+            return RequestConfiguration(
+                method: .get,
+                path: mainRoute.appending("\(id)"),
+                parameters: parameters,
+                encoding: URLEncoding.default,
+                language: language
+            )
+        case let .similar(auth, id, page, language):
+            var parameters: [String: Any] = ["api_key": auth]
+            if let page = page {
+                parameters["page"] = page
+            }
+            if let language = language {
+                parameters["language"] = language
+            }
+            return RequestConfiguration(
+                method: .get,
+                path: mainRoute.appending("\(id)/").appending("similar"),
+                parameters: parameters,
+                encoding: URLEncoding.default,
+                language: language
+            )
+        }
+    }
+}
